fe256608cb81c3f68764b41756128418133fdb79
Роман Джихур <rom3053@gmail.com>
2020-08-29 17:10:28 +0300

    Update

---
+++ VideoLibrary.API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoLibrary.API.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
