20523bf408a8ae7ea39733cac7228a22cd10a80f
Роман Джихур <rom3053@gmail.com>
2020-09-18 16:27:26 +0300

    update_18092020

---
+++ VideoLibrary.Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using VideoLibrary.API.Models;
+
+namespace VideoLibrary.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+    }
+}
---
+++ VideoLibrary.Models/Submission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoLibrary.API.Models
+{
+    public class Submission
+    {
+        public int Id { get; set; }
+        public int VideoId { get; set; }
+        public string VideoFile { get; set; }
+        public string Description { get; set; }
+    }
+}
