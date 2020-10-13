66f8aa9c6873fbb3c58b9f74f4aa69abbb8f5e48
Роман Джихур <rom3053@gmail.com>
2020-10-13 14:13:20 +0300

    Update-13.10.2020 Add change source for video

--- web-client/pages/videos/_videoSubmission.vue
+++ web-client/pages/videos/_videoSubmission.vue
@@ -3,12 +3,13 @@
       <div>
             Video: {{$route.params.videoSubmission }}
       </div> 
-
+    <v-btn @click="changeVideo">CHANGE VIDEO</v-btn>
     <div v-if="submissions">
       <div v-bind:key="s" v-for="s in submissions">
-        {{s.id}} - {{s.description}} - {{s.videoId}}
+        {{s.id}} - {{s.description}} - {{s.videoId}} -
+        {{videolinkFirst}} --{{videolinkSecond}}
         <div>
-          <video width="400" :src="`http://localhost:5000/api/videoFiles/${s.videoFile}`" controls>
+          <video width="400" :src="video" controls>
             
 
           </video>
@@ -24,11 +25,27 @@
 <script>
 import {mapState} from 'vuex';
 
-export default {
+  export default {
+    data: ()=> ({
+      videolinkSecond: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+      videolinkFirst: "http://localhost:5000/api/videoFiles/",
+      video:"",
+    }),
+    methods: {
+      changeVideo() {
+        if (this.video == this.videolinkFirst) {
+          this.video = this.videolinkSecond
+        } else {
+          this.video = this.videolinkFirst
+        }
+      }
+    },
     computed: mapState('submissions',['submissions']),
     async fetch(){
         const videoId = this.$route.params.videoSubmission;
-        await this.$store.dispatch("submissions/fetchSubmissions", {videoId}, {root: true})
+      await this.$store.dispatch("submissions/fetchSubmissions", { videoId }, { root: true })
+      this.videolinkFirst += this.submissions[0].videoFile
+      this.video = this.videolinkFirst
     }
 }
 </script>
