aaeaeb7001e4b2fbb0fa23ce0e33a8a21fbb1181
Роман Джихур <rom3053@gmail.com>
2020-12-03 21:21:08 +0200

    Update-03.12.2020 Add logical for convertation to different quality

--- VideoLibrary.API/BackgroundServices/VideoEditingBackgroundService.cs
+++ VideoLibrary.API/BackgroundServices/VideoEditingBackgroundService.cs
@@ -43,91 +43,208 @@ namespace VideoLibrary.API.BackgroundServices
                 string executablesPath = Path.Combine(_env.ContentRootPath, "FFmpeg");
                 FFmpeg.SetExecutablesPath(executablesPath);
 
+                using var scope = _serviceProvider.CreateScope();
+                var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                var submission = ctx.Submissions.FirstOrDefault(x => x.Id.Equals(message.SubmissionId));
+                var video = ctx.Videos.FirstOrDefault(v => v.Id.Equals(submission.VideoId));
+
                 string previewImageName = Guid.NewGuid() + ".png";
+                video.PreviewImage = previewImageName;
+
                 try
                 {
-
-                    
-
                     #region ConvertRegion
-                    
-                    
 
-                    string convertFileName = message.Output + "FFmpeg" + ".mp4";
+                    string convertFileName = message.Output;
                     string outputPath = Path.Combine(_env.WebRootPath, convertFileName);
                     string inputPath = Path.Combine(_env.WebRootPath, message.Input);
-                    
+                    string outputName = string.Empty;
 
 
                     IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
-                    
-                    IStream videoStream = mediaInfo.VideoStreams.FirstOrDefault()
-                        ?.SetCodec(VideoCodec.h264_nvenc)
-                        ?.SetSize(VideoSize.Hd480)
-                        ?.SetFramerate(24);
-                    
-                    IStream audioStream = mediaInfo.AudioStreams.FirstOrDefault()
-                        ?.SetCodec(AudioCodec.aac);
-                    
 
-                    string outputSnapshot = Path.Combine(_env.WebRootPath, previewImageName);
-                    IConversion conversionSnapshot =  FFmpeg.Conversions.New().AddParameter($"-i {inputPath} -ss {TimeSpan.FromSeconds(0)}" +
-                                                                                            $" -vframes 1 -vf scale=1920x1080 {outputSnapshot}");
-                    IConversionResult resultSnapshot = await conversionSnapshot.Start();
+                    await GetSnapshot(previewImageName, inputPath);
 
-
-
-                    IConversion conversion = FFmpeg.Conversions.New();
-                    conversion.OnProgress += (sender, args) =>
+                    ConvertOption videoSize = GetConvertOptionMaxQualityToConvert(mediaInfo);
+                    switch (ConvertOption.Option240)
                     {
-                        var percent = (int)(Math.Round(args.Duration.TotalSeconds / args.TotalLength.TotalSeconds, 2) * 100);
-                        Console.WriteLine($"[{args.Duration} / {args.TotalLength}] {percent}%");
-                    };
-
-                    IConversionResult conversionResult = await conversion
-                        .AddParameter("-hwaccel_device 0 -hwaccel cuda", ParameterPosition.PreInput)
-                        .AddStream(audioStream, videoStream)
-                        .AddParameter("-strict -2", ParameterPosition.PostInput)
-                        .SetOutput(outputPath)
-                        .Start();
-
-                    Console.WriteLine("\nDuration: " + conversionResult.Duration+"\nCommand: "+conversionResult.Arguments+"\n");
+                        case ConvertOption.Option1080:
+                            IConversionResult conversionResult_1080 = await ConversionProcessWithAlterableQuality(outputPath, mediaInfo, VideoSize.Hd1080);
+                            DisplayResultToConsole(conversionResult_1080);
+                            await AddQualityToDatabase(message, ctx, submission, stoppingToken, VideoSize.Hd1080);
+                            if (videoSize < ConvertOption.Option1080)
+                            {
+                                //goto case ConvertOption.Option360;
+                            }
+                            DeleteSourceFile(inputPath);
+                            break;
+                        case ConvertOption.Option720:
+                            IConversionResult conversionResult_720 = await ConversionProcessWithAlterableQuality(outputPath, mediaInfo, VideoSize.Hd720);
+                            DisplayResultToConsole(conversionResult_720);
+                            await AddQualityToDatabase(message, ctx, submission, stoppingToken, VideoSize.Hd720);
+                            if (videoSize < ConvertOption.Option720)
+                            {
+                                goto case ConvertOption.Option1080;
+                            }
+                            DeleteSourceFile(inputPath);
+                            break;
+                        case ConvertOption.Option480:
+                            IConversionResult conversionResult_480 = await ConversionProcessWithAlterableQuality(outputPath, mediaInfo, VideoSize.Hd480);
+                            DisplayResultToConsole(conversionResult_480);
+                            await AddQualityToDatabase(message, ctx, submission, stoppingToken, VideoSize.Hd480);
+                            if (videoSize < ConvertOption.Option480)
+                            {
+                                goto case ConvertOption.Option720;
+                            }
+                            DeleteSourceFile(inputPath);
+                            break;
+                        case ConvertOption.Option360:
+                            IConversionResult conversionResult_360 = await ConversionProcessWithAlterableQuality(outputPath, mediaInfo, VideoSize.Nhd);
+                            DisplayResultToConsole(conversionResult_360);
+                            await AddQualityToDatabase(message, ctx, submission, stoppingToken, VideoSize.Nhd);
+                            if (videoSize < ConvertOption.Option360)
+                            {
+                                goto case ConvertOption.Option480;
+                            }
+                            DeleteSourceFile(inputPath);
+                            break;
+                        case ConvertOption.Option240:
+                            IConversionResult conversionResult_240 = await ConversionProcessWithAlterableQuality(outputPath, mediaInfo, VideoSize.Fwqvga);
+                            DisplayResultToConsole(conversionResult_240);
+                            string videoLink = message.Output + "FFmpeg_" + VideoSize.Fwqvga + ".mp4";
+                            submission.VideoFile = videoLink;
+                            VideoQuality videoQuality = new VideoQuality
+                            {
+                                QualityName = "240",
+                                Submission = submission,
+                                SubmissionId = submission.Id,
+                                QualityVideoLink = videoLink
+                            };
+                            submission.VideoQualities.Add(videoQuality);
+                            await ctx.SaveChangesAsync(stoppingToken);
+                            if (videoSize < ConvertOption.Option240)
+                            {
+                                goto case ConvertOption.Option360;
+                            }
+                            DeleteSourceFile(inputPath);
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
                     _logger.LogError(e, "Video Processing Failed for {0}", message.Input);
                 }
                 #endregion
-
-                using (var scope = _serviceProvider.CreateScope())
-                {
-                    var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-                    var submission = ctx.Submissions.FirstOrDefault(x => x.Id.Equals(message.SubmissionId));
-                    var video = ctx.Videos.FirstOrDefault(v => v.Id.Equals(submission.VideoId));
-                    video.PreviewImage = previewImageName;
-                    
-                    submission.VideoFile = message.Output + "FFmpeg" + ".mp4";
-                    submission.VideoProcessed = true;
-                    VideoQuality videoQuality = new VideoQuality
-                    {
-                        QualityName = "480",
-                        Submission = submission,
-                        SubmissionId = submission.Id,
-                    };
-                    VideoQuality videoQuality1 = new VideoQuality
-                    {
-                        QualityName = "1080",
-                        Submission = submission,
-                        SubmissionId = submission.Id,
-                    };
-                    //ctx.VideoQualities.Add(videoQuality);
-                    submission.VideoQualities.Add(videoQuality1);
-                    submission.VideoQualities.Add(videoQuality);
-                    await ctx.SaveChangesAsync(stoppingToken);
-                }
             }
         }
+
+        private static async Task<IConversionResult> ConversionProcessWithAlterableQuality(string outputPath, IMediaInfo mediaInfo, VideoSize videoSize)
+        {
+            IStream videoStream = mediaInfo.VideoStreams.FirstOrDefault()
+                                    ?.SetCodec(VideoCodec.h264_nvenc)
+                                    ?.SetSize(videoSize)
+                                    ?.SetFramerate(24);
+
+            IStream audioStream = mediaInfo.AudioStreams.FirstOrDefault()
+                ?.SetCodec(AudioCodec.aac);
+
+
+
+            IConversion conversion = FFmpeg.Conversions.New();
+            conversion.OnProgress += (sender, args) =>
+            {
+                var percent = (int)(Math.Round(args.Duration.TotalSeconds / args.TotalLength.TotalSeconds, 2) * 100);
+                Console.WriteLine($"[{args.Duration} / {args.TotalLength}] {percent}%");
+            };
+
+            outputPath += "FFmpeg_" + videoSize + ".mp4";
+            IConversionResult conversionResult = await conversion
+                .AddParameter("-hwaccel_device 0 -hwaccel cuda", ParameterPosition.PreInput)
+                .AddStream(audioStream, videoStream)
+                .AddParameter("-strict -2", ParameterPosition.PostInput)
+                .SetOutput(outputPath)
+                .Start();
+            return conversionResult;
+        }
+
+        private static async Task AddQualityToDatabase(EditVideoMessage message, AppDbContext ctx, Submission submission, CancellationToken stoppingToken, VideoSize videoSize)
+        {
+            string videoLink = message.Output + "FFmpeg_" + videoSize + ".mp4";
+            VideoQuality videoQuality = new VideoQuality
+            {
+                QualityName = videoSize.ToString(),
+                Submission = submission,
+                SubmissionId = submission.Id,
+                QualityVideoLink = videoLink
+            };
+            submission.VideoQualities.Add(videoQuality);
+            await ctx.SaveChangesAsync(stoppingToken);
+        }
+
+        private static ConvertOption GetConvertOptionMaxQualityToConvert(IMediaInfo mediaInfo)
+        {
+            ConvertOption videoSize = ConvertOption.Option240;
+            int videoHeight = mediaInfo.VideoStreams.FirstOrDefault().Height;
+            if (videoHeight >= 1080)
+            {
+                videoSize = ConvertOption.Option1080;
+                return videoSize;
+            }
+            else if (videoHeight < 1080 && videoHeight >= 720)
+            {
+                videoSize = ConvertOption.Option720;
+                return videoSize;
+            }
+            else if (videoHeight < 720 && videoHeight >= 480)
+            {
+                videoSize = ConvertOption.Option480;
+                return videoSize;
+            }
+            else if (videoHeight < 480 && videoHeight >= 360)
+            {
+                videoSize = ConvertOption.Option360;
+                return videoSize;
+            }
+            else if (videoHeight < 359)
+            {
+                videoSize = ConvertOption.Option240;
+                return videoSize;
+            }
+            return videoSize;
+        }
+
+        private static void DisplayResultToConsole(IConversionResult conversionResult)
+        {
+            Console.WriteLine("\nDuration: " + conversionResult.Duration + "\nCommand: " + conversionResult.Arguments + "\n");
+        }
+
+        private async Task GetSnapshot(string previewImageName, string inputPath)
+        {
+            string outputSnapshot = Path.Combine(_env.WebRootPath, previewImageName);
+            IConversion conversionSnapshot = FFmpeg.Conversions.New().AddParameter($"-i {inputPath} -ss {TimeSpan.FromSeconds(0)}" +
+                                                                                    $" -vframes 1 -vf scale=800x400 {outputSnapshot}");
+            IConversionResult resultSnapshot = await conversionSnapshot.Start();
+        }
+
+        private static void DeleteSourceFile(string inputPath)
+        {
+            if (File.Exists(inputPath))
+            {
+                File.Delete(inputPath);
+            }
+        }
+    }
+    public enum ConvertOption
+    {
+        Option1080 = 1,
+        Option720,
+        Option480,
+        Option360,
+        Option240
+            
     }
     public class EditVideoMessage
     {
